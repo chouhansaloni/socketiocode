01cbe884c7395e63d47580f3d0467473470325b0
saloni <salonichouhan0806@gmail.com>
2025-01-03 16:44:19 +0530

    .

--- Models/conversation.model.js
+++ Models/conversation.model.js
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose';
 
 const conversationSchema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
-  lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
-  updatedAt: { type: Date, default: Date.now },
+  participants: [{ 
+    type: mongoose.Schema.Types.ObjectId,
+     ref: 'User',
+      required: true 
+    }],
+  lastMessage: { 
+    type: mongoose.Schema.Types.ObjectId,
+     ref: 'Message' 
+    },
+  updatedAt: {
+     type: Date, 
+     default: Date.now },
 });
 
 export default mongoose.model('Conversation', conversationSchema);
